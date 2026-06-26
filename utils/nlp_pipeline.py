89b77dccf80137711cc39f83842e821c55db6a98
DHANASRI.R <rdhanasri416@gmail.com>
2026-06-26 20:40:01 +0530

    Fix spaCy model installation

--- utils/nlp_pipeline.py
+++ utils/nlp_pipeline.py
@@ -1,12 +1,7 @@
 import spacy
 
 # Load the English model
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    from spacy.cli import download
-    download("en_core_web_sm")
-    nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")
 
 def preprocess_nlp(text):
     doc = nlp(text)
